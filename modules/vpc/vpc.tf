2ee3702ba369df8160ae05585f57c8ddbcf606a7
Sami Sallam <ssallam@clinomic.ai>
2025-03-07 13:32:32 +0100

    Add ignore_changes feature

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -2,6 +2,13 @@ resource "opentelekomcloud_vpc_v1" "vpc" {
   name = var.name
   cidr = var.cidr_block
   tags = var.tags
+
+    lifecycle {
+    ignore_changes = [
+      cidr
+    ]
+  }
+
 }
 
 resource "opentelekomcloud_vpc_subnet_v1" "subnets" {
@@ -12,4 +19,11 @@ resource "opentelekomcloud_vpc_subnet_v1" "subnets" {
   gateway_ip = cidrhost(each.value != "" ? each.value : local.subnets_default_cidr[each.key], 1)
   dns_list   = var.dns_config
   tags       = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      cidr
+    ]
+  }
+
 }
